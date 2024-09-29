4e116272bc617f564c8e619c3ac088cdfbe1d1fa
cybersafe-security <cybersafe.security@proton.me>
2024-09-29 21:59:16 +0200

    build

--- src/lib/components/atoms/Image.svelte
+++ src/lib/components/atoms/Image.svelte
@@ -1,24 +1,17 @@
 <script lang="ts">
 	import { dev } from '$app/environment';
-
 	export let src: string;
 	export let alt: string;
 	export let fullBleed: boolean | undefined = undefined;
-
 	export let formats: string[] = ['avif', 'webp', 'png'];
 	export let widths: string[] | undefined = undefined;
-
 	$: fileName = src.split('.')[0];
-
 	function buildSrcset() {
 		if (dev) return;
-
 		let srcset = '';
-
 		if (widths) {
 			for (let i = 0; i < widths.length; i++) {
 				srcset += `${fileName}-${widths[i]}.${formats[0]} ${widths[i]}w`;
-
 				if (i < widths.length - 1) {
 					srcset += ', ';
 				}
@@ -26,23 +19,38 @@
 		} else {
 			for (let i = 0; i < formats.length; i++) {
 				srcset += `${fileName}.${formats[i]}`;
-
 				if (i < formats.length - 1) {
 					srcset += ', ';
 				}
 			}
 		}
-
 		return srcset;
 	}
 </script>
 
-<img srcset={buildSrcset()} {src} {alt} loading="lazy" decoding="async" class:full-bleed={fullBleed} />
+<div class="image-container" class:full-bleed={fullBleed}>
+	<img srcset={buildSrcset()} {src} {alt} loading="lazy" decoding="async" />
+</div>
 
 <style lang="scss">
-	img {
+	.image-container {
 		width: 100%;
 		height: 100%;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		overflow: hidden;
+	}
+
+	.full-bleed {
+		width: 100vw;
+		margin-left: 50%;
+		transform: translateX(-50%);
+	}
+
+	img {
+		max-width: 100%;
+		max-height: 100%;
 		object-fit: contain;
 	}
 </style>
