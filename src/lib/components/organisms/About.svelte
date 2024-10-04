10dd9c36425f8ad4f9e851ae1c31429d545c87ad
cybersafe-security <cybersafe.security@proton.me>
2024-10-04 13:54:14 +0200

    build

--- src/lib/components/organisms/About.svelte
+++ src/lib/components/organisms/About.svelte
@@ -95,7 +95,7 @@
 	</Card.Root>
 		
 	</div>
-	<div class="image" style="background: rgba(0, 0, 0, 0.6);border:solid 1px #6f147f;box-shadow:0px 0px 10px black;">
+	<div class="image">
 		<Image src="/images/cs-mobile-degoogliser.jpg" alt="Sample for the static template" />
 	</div>
 </section>
